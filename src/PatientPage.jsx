2966f48ed3aad654b8570541803d43d4f1d98dab
Aurelia Sindhu <100260518+AureliaSindhu@users.noreply.github.com>
2024-04-28 04:06:53 -0700

    update patientinfo

--- src/PatientPage.jsx
+++ src/PatientPage.jsx
@@ -2,10 +2,19 @@ import React from 'react'
 
 const PatientPage = ( {setDisplay} ) => {
   return (
-    <div>
-      <button onClick={() => setDisplay("profile")}>Edit Profile</button>
-      <button onClick={() => setDisplay("records")}>Load Records</button>
-      <button onClick={() => setDisplay("login")}>Logout</button>
+    <div className='container d-flex flex-column align-items-center' style={{marginTop: '10%'}} >
+      <div className='title'>
+        <h1> Menu </h1>
+      </div>
+      <div className='pBut'>
+        <button onClick={() => setDisplay("profile")} className='navBut'>Edit Profile</button>
+      </div>
+      <div className='pBut'>
+        <button onClick={() => setDisplay("records")} className='navBut'>Load Records</button>
+      </div>
+      <div className='pBut'>
+        <button onClick={() => setDisplay("login")} className='navBut'>Logout</button>
+      </div>
     </div>
   )
 }
